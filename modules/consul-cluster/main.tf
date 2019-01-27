708e6a1dbc97663ec9c879c0ee47c4562190a7e4
stoffee <chris@roadkill.org>
2019-01-27 11:36:00 -0800

    added http inbound

--- modules/consul-cluster/main.tf
+++ modules/consul-cluster/main.tf
@@ -217,7 +217,7 @@ resource "azurerm_network_security_rule" "ssh" {
   direction = "Inbound"
   name = "SSH${count.index}"
   network_security_group_name = "${azurerm_network_security_group.consul.name}"
-  priority = "${100 + count.index}"
+  priority = "100"
   protocol = "Tcp"
   resource_group_name = "${var.resource_group_name}"
   source_address_prefix = "${element(var.allowed_ssh_cidr_blocks, count.index)}"
